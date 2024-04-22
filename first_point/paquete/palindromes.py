30b0a6a05578967454761d129cbd733836306a60
julian gonzalez muñoz <julgonzalezmu@unal.edu.co>
2024-04-22 14:41:11 -0500

    adding exceptions in palindromes

--- first_point/paquete/palindromes.py
+++ first_point/paquete/palindromes.py
@@ -1,14 +1,20 @@
 def is_palindrome(word: str) -> bool:
-    flag = True
-    if len(word) % 2 != 0:
-        for char in range(len(word)):
-            if char != (len(word)//2)+1:
+    try:
+        flag = True
+        if len(word) % 2 != 0:
+            for char in range(len(word)):
+                if char != (len(word)//2)+1:
+                    if word[char] != word[-char-1]:
+                        flag = False
+                        break
+        else:
+            for char in range(len(word)):
                 if word[char] != word[-char-1]:
                     flag = False
                     break
-    else:
-        for char in range(len(word)):
-            if word[char] != word[-char-1]:
-                flag = False
-                break
-    return flag
+        return flag
+    except TypeError:
+        return "invalid Input"
+
+
+print(is_palindrome(123))
