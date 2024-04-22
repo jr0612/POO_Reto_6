f149b2dad47d1cb2da89c773f6dabca5605a36af
julian gonzalez muñoz <julgonzalezmu@unal.edu.co>
2024-04-22 14:15:18 -0500

    adding exceptions in operations

--- first_point/paquete/operations.py
+++ first_point/paquete/operations.py
@@ -1,4 +1,4 @@
-def basic_operation(first_number: int,  second_number: int, sign: str):
+def basic_operation(first_number: int,  second_number: int, sign: chr):
     match sign:
         case  "+":
             return first_number + second_number
@@ -7,4 +7,29 @@ def basic_operation(first_number: int,  second_number: int, sign: str):
         case  "*":
             return first_number * second_number
         case  "/":
-            return first_number / second_number
+            try:
+                return first_number / second_number 
+            except ZeroDivisionError:
+                 return "Math ERROR"   
+        case _:
+            return 'invalid operation' #* adding the default case of match statement
+        
+def perform_operation(first_number: int , second_number: int, sign: chr):
+    '''
+    Catching the TypeError
+    '''
+    try:
+        return basic_operation(first_number, second_number, sign)
+    except TypeError:
+        return "Invlid input type"
+    
+
+'''
+Trying better coments:
+#* important information
+#! warning
+# ? questions 
+# todo 
+'''
+
+print(perform_operation(1,'6','+'))
