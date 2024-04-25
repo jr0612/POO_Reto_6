d36fb673ee71d31bb8ada1940ac4d8ff2af30827
Ricardo Gonzalez <julgonzalezmu@unal.edu.co>
2024-04-24 20:33:07 -0500

    adding exceptions to anagrams.py

--- first_point/paquete/anagrams.py
+++ first_point/paquete/anagrams.py
@@ -1,4 +1,20 @@
 def find_anagrams(words: list) -> list:
+    """
+    a function that finds anagrams from a list of words
+    param: words: list
+    return: anagrams: list
+    """
+    # ! raise error
+    if not isinstance(words, list):
+        # raise error for not list input
+        raise TypeError("the input is not a list")
+    if not words:
+        # raise error for empty list
+        raise ValueError("the input list is empty")
+    if not all(word.isalpha() for word in words):
+        # raise error for non-alphabetic characters
+        raise ValueError("the input list contains non-alphabetic characters")
+
     anagrams_dict = {}
     for word in words:
         sort_word = "".join(sorted(word))
@@ -6,5 +22,21 @@ def find_anagrams(words: list) -> list:
             anagrams_dict[sort_word].append(word)
         else:
             anagrams_dict[sort_word] = [word]
-    anagrams = [word for group in anagrams_dict.values() if len(group) > 1 for word in group]
+    anagrams = [
+        word for group in anagrams_dict.values() if len(group) > 1 for word in group
+    ]
     return anagrams
+
+
+def perform_find_anagrams(words):
+    try:
+        return find_anagrams(words)
+    except Exception as identifier:
+        print("error: ", identifier)
+
+
+print(perform_find_anagrams(["hello", "world", "hello"]))
+print(perform_find_anagrams(["dog", "god", "cat"]))
+print(perform_find_anagrams(["ab", "123"]))
+print(perform_find_anagrams([]))
+print(perform_find_anagrams(134))
