7f61b6c7920fe1e8d2051cd93739f907da84d94e
Ricardo Gonzalez <julgonzalezmu@unal.edu.co>
2024-05-12 21:46:40 -0500

    Adding adding exceptions for the creation of triangles

--- second_point/shape/triangle.py
+++ second_point/shape/triangle.py
@@ -1,9 +1,12 @@
 import math
 
-from shape.shape import Shape
+from shape.shape import Shape, Point, Line
 
 
 class Triangle(Shape):
+    def __init__(self):
+        self.frec = {}
+
     def compute_area(self):
         # Heron's formula is used
         edge_a = self.get_edges()[0].get_length()
@@ -39,29 +42,84 @@ class Triangle(Shape):
         )
         angle_C = 180 - angle_A - angle_B
 
-        self.set_inner_angles([(angle_A), (angle_B), (angle_C)])
+        self.set_inner_angles([round(angle_A), round(angle_B), round(angle_C)])
+
+    # todo implement method to set Triangle vertices correctly
+    def create_vertices(self, vertices: list):
+        if len(vertices) != 3:
+            raise ValueError(
+                "Triangle must have 3 vertices"
+                # ! raise error if vertices are not 3
+            )
+        if not all(isinstance(vertex, Point) for vertex in vertices):
+            raise TypeError(
+                "Triangle vertices must be Point objects"
+                # ! raise error if vertices are not Point objects
+            )
+        side_lengths = [
+            vertices[0].compute_distance(vertices[1]),
+            vertices[1].compute_distance(vertices[2]),
+            vertices[2].compute_distance(vertices[0]),
+        ]
+        for side_length in side_lengths:
+            if side_length not in self.frec:
+                self.frec[side_length] = 1
+            else:
+                self.frec[side_length] += 1
+
+    # todo implement method to set Triangle edges correctly
+    def create_edges(self, edges: list):
+        if len(edges) != 3:
+            raise ValueError(
+                "Triangle must have 3 edges"
+                # ! raise error if edges are not 3
+            )
+        if not all(isinstance(edge, Line) for edge in edges):
+            raise TypeError(
+                "Triangle edges must be Line objects"
+                # ! raise error if edges are not Line objects
+            )
+        if self.get_vertices():
+            self.set_edges(edges)
+        else:
+            raise ValueError(
+                "Triangle edges are not correct"
+                # raise error if edges are not correct
+            )
 
 
 class Isosceles(Triangle):
     # todo implement method to set Isoceles vertices correctly
-    # todo implement method to set Isoceles edges correctly
-
-    pass
+    def create_vertices(self, vertices: list):
+        super().create_vertices(vertices)
+        if any(count == 2 for count in self.frec.values()):
+            self.set_vertices(vertices)
+            return True
+        else:
+            raise ValueError("Isosceles must have 2 equal sides")
 
 
 class Equilateral(Triangle):
     # todo implement method to set Equilateral vertices correctly
-    # todo implement method to set Equilateral edges correctly
-    pass
+    def create_vertices(self, vertices: list):
+        super().create_vertices(vertices)
+        if all(count == 3 for count in self.frec.values()):
+            self.set_vertices(vertices)
+            return True
+        else:
+            raise ValueError("Equilateral must have 3 equal sides")
 
 
 class Scalene(Triangle):
     # todo implement method to set Scalene vertices correctly
-    # todo implement method to set Scalene correctly
-    pass
+    def create_vertices(self, vertices: list):
+        super().create_vertices(vertices)
+        if all(count == 1 for count in self.frec.values()):
+            self.set_vertices(vertices)
+            return True
+        else:
+            raise ValueError("Scalene must have 3 unequal sides")
 
 
 class TriRectangle(Triangle):
     # todo implement method to set TriRectangle vertices correctly
-    # todo implement method to set TriRectangle edges correctly
-    pass
